1ec96388e0b86f351ad3d5e773c4074ca26c200c
AViralMushroom <altimepro78@gmail.com>
2024-02-09 13:26:35 -0800

    deleted everything to restart, files on replit

--- Main.java
+++ Main.java
@@ -1,37 +1,5 @@
-import java.awt.*;
-import java.util.Scanner;
-import java.io.File;
-import java.util.ArrayList;
-import java.io.FileNotFoundException;
-class Main {
-  public static void main(String[] args) {
-
-    Display screen = new Display(640, 360, "buh");
-    screen.paint();
-    String[] secrets = loadGameSecrets("GameSecrets.csv");
-    System.out.println("////// Congrats your code works //////");
-  }
-
-  public static String[] loadGameSecrets(String filename) {
-    ArrayList<String> secretsList = new ArrayList<>();
-
-    try {
-      Scanner fileScanner = new Scanner(new File(filename));
-
-      while (fileScanner.hasNextLine()) {
-        String line = fileScanner.nextLine();
-        secretsList.add(line);
-      }
-
-      fileScanner.close();
-    } catch (FileNotFoundException e) {
-      System.err.println("File not found: " + filename);
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Hello world");
     }
-
-    // Convert ArrayList to String array
-    String[] secretsArray = new String[secretsList.size()];
-    secretsArray = secretsList.toArray(secretsArray);
-
-    return secretsArray;
-  }
 }
