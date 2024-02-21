b8960f8a911edc0c7e4da9bc0930eb00f67c61a7
GalaxyJumper <82897669+GalaxyJumper@users.noreply.github.com>
2024-02-21 14:17:15 -0800

    Updated Gui

--- Gui.java
+++ Gui.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
 public class Gui extends JPanel{
     public Gui(){
-        
+        JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addKeyListener(panel);
+        frame.add(panel);
+        frame.pack();
+
+        frame.setTitle(name);
+        frame.setVisible(true);
     }
 }
