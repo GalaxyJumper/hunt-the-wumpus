4a4bece84268e1d0427ecb1e6353d00f9307e83b
GalaxyJumper <82897669+GalaxyJumper@users.noreply.github.com>
2024-02-25 21:51:58 +0000

    Updated Gui and added "no errors" message

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main (String[] args){
         System.out.println("Hello world");
+        Gui gui = new Gui("HUNT THE WUMPUS", 960, 540);
         //bonjour
+        System.out.println("Your code works! 🎉");
     }
 }
