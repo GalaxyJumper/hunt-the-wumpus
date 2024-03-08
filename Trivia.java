8f05dbf5cda98357ccda1f1b34f091e6d692006f
Stultus757 <1053757@apps.nsd.org>
2024-03-08 13:14:22 -0800

    DDDDDD

--- Trivia.java
+++ Trivia.java
@@ -6,21 +6,25 @@ class Trivia{
     // PROPERTIES --------------------
     File trivia = new File("TriviaQ.csv");
     Scanner triv = new Scanner("trivia");
-    int genre = 0;
+    String genre = "";
     Boolean rightflag;
     // 0 - trivia, 1 - 2 - 3 - 4 - ect ill update this later
 
     // CONSTRUCTOR --------------------
-public Trivia(int genre){
+public Trivia(String genre){
     this.genre = genre;
 }
     // Methoods -----------------------
     
     //Takes the trivia question and all of its properties
+    //the MAIN function of trivia.
     //then, asks them for an answer and returns a flag wether their answer is right or not.
     //Using this flag, they can determine to give the reward or the consequence based on which object
     //is calling Triviarun
+
+    //Split this into multiple objects? Getters and setters
 public Boolean Triviarun(){
+    System.out.println("This program works! - Triviarun");
     //WEIOUIL HEOIHLHASO DJADASDCFEFSDC
     return rightflag;
 }
