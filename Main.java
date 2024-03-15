50238b50b5c9ee50903ca04ff73fc2c03bcac89d
GalaxyJumper <82897669+GalaxyJumper@users.noreply.github.com>
2024-03-15 13:36:24 -0700

    Attempted drawHex method

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) throws HeadlessException{
         Scanner scan = new Scanner(System.in);
-        System.out.println("Is this program running in Replit? (y/n)");
-        boolean workingInReplit = scan.next().equals("y");
-        if (workingInReplit){ 
+
+        System.out.println("Is this program running in Github Codepaces? (y/n)");
+        boolean workingInCodespaces = scan.next().equals("y");
+        if (!workingInCodespaces){ 
             Gui gui = new Gui("HUNT THE WUMPUS", 960, 540); 
         }
+
         //bonjour 
         System.out.println("Your code works ~*,`");
     }
