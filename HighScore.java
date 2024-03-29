e930f1af6443839a1d496378e341b267e4bac157
kavya-tayall <2005879@apps.nsd.org>
2024-03-29 12:57:39 -0700

    yay

--- HighScore.java
+++ HighScore.java
@@ -2,17 +2,33 @@
 //02/12/24
 //Scores Object
 
-    import java.io.File;
-    import java.io.*;
-    public class HighScore {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JFileChooser;
+
+public class HighScore {
     ///Properties
 
 
     ///Constructors
     public HighScore(){
+    
+        //String path = GetDefaultLogFileName();
+        //File highScoreFile = new File(path);
+
+        
+
+
         //File HighScore = new File("Highscore.csv");
         //BufferedWriter bw = new BufferedWriter(fw);
-         //BufferedReader br = new BufferedReader(fw);
+        //BufferedReader br = new BufferedReader(fw);
         //Filewriter fw = new FileWriter("Highscore.csv",true);
 
     }
@@ -20,11 +36,14 @@
 
 
     ///Methods
+    //private static String GetDefaultLogFileName() {
+        //return System.getProperty(C:\git\hunt-the-wumpus-1\Highscore.csv);
+       // }
 
     //checks if the current player has a high score, updates data table, and displays data table
     public void endOfGame(Player player){
         endOfGameCheck(player);
-        endOfGameUpdate(player);
+        if(endOfGameCheck(player)) endOfGameUpdate(player);
         displayTable();
     }
     
