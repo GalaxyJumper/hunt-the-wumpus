fde67cf39cef56cf1f36f8f116599d503ffdd7a7
GalaxyJumper <82897669+GalaxyJumper@users.noreply.github.com>
2024-04-05 13:36:25 -0700

    drawHexGrid

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) throws HeadlessException, FileNotFoundException{
         Scanner scan = new Scanner(System.in);
-
+        //important anti error for codespaces
         System.out.println("Is this program running in Github Codepaces? (y/n)");
         boolean workingInCodespaces = scan.next().equals("y");
         if (!workingInCodespaces){ 
-            Gui gui = new Gui("HUNT THE WUMPUS", 960, 540); 
+            Gui gui = new Gui("HUNT THE WUMPUS", 2560, 1440); 
         }
 
         Trivia trivia = new Trivia();
