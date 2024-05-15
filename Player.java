0c4f5d572b21b8510362c6cbe737b0d8548f51c0
NickLennon <1085280@apps.nsd.org>
2024-05-15 12:55:24 -0700

    Building out player class

--- Player.java
+++ Player.java
@@ -1,19 +1,13 @@
 /*
  * Keeps track of player inventory: Arrows and Gold Coins
- * Keeps track of how many truns the player has taken
- * Computes  the ending score of player
+ * Keeps track of how many turns the player has taken
+ * Computes the ending score of player
  */
 
 // Joshua Lennon
 // Nick Lennon
 // 2/12/2024
 // Player Object
-
-//Make player a static class (singleton class)
-//Static variables have space in memory set aside
-//So all classes are referencing the same object 
-//and chainging it without needing to communicate with each other
-//I made a static player object in player
 
 public class Player{
     
@@ -35,36 +29,58 @@ public class Player{
         this.arrows = 3;
         this.coins = 0;
         this.turnsTaken = 0;
+        this.score = 0;
     }
 
     ///////////////////////////////////////////////
     // METHODS
     ///////////////////////////////////////////////
 
-    public Player getPlayer(){
-        return this;
-    }
-
-
+    //This method will be called in gameControl to check the validity of a move
+    //and also update the location of the player on the CSV
     public void move(){
         turnsTaken++;
     }
 
+    //This method will be called in gameControl to determine the validity of a shot
+    //And check if the wumpus is in the adjacent room
+    //If the wumpus is in the other room, gameControl will then call another method relating to a hit
     public void shootArrow(){
-        this.arrows--;
+        if (this.arrows > 0){
+            this.arrows--;
+        } else {
+            System.out.println("My quiver is empty!");
+        }
+
     }
 
+    //This method will be called in gameControl through an input in store.
+    //The effect is just exchanging a coin for an arrow
+    //We need more complex stuff in the GUI
     public void purchaseArrow(){
         this.coins--;
         this.arrows++;
     }
  
+    //We need our trivia class to be fully complete for this method to work
+    //TBD
     public void purchaseSecret(){
 
     }
 
+    //Currently the score of a player is determined by multiplying coins by 10
+    //TBD
     public int calcScore(){
-        return 0;
+    this.score = this.coins * 10;
+        return this.score;
+    }
+
+////////////////////////////////////
+//GETTERS AND SETTERS
+////////////////////////////////////
+
+    public Player getPlayer(){
+        return this;
     }
 
     public int getArrows() {
