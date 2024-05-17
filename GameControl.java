812cf856bcff954bd90bb8ccf847f3c27d5c9093
AViralMushroom <altimepro78@gmail.com>
2024-05-17 13:37:36 -0700

    added useful stuff

--- GameControl.java
+++ GameControl.java
@@ -1,37 +1,32 @@
-// Joshua Lennon
-// Nick Lennon
+// Joshua Lennon (cringe)
+// Nick Lennon (less cringe)
+// Aviral Mishra (the cool one)
 // Toki
-// 2/12/2024
+// 2/12/2024lkjh
 // Game Control Object
 
 //taking in inputs for all classes and running the game
 //connecting the ui to the game itself
 import java.util.Scanner;
+import java.awt.GraphicsEnvironment;
 
 public  class GameControl {
     ///////////////////////////////////////////////
     // VARIABLES
     ///////////////////////////////////////////////
 
-    private Player player;
-    private Trivia trivia;
-    private Wumpus wumpus;
-    private boolean playerInTrap = false;
     private GameLocations gameLocs;
-    private Cave cave;
     private Gui gui;
-
 
     ///////////////////////////////////////////////
     // CONSTRUCTORS
     ///////////////////////////////////////////////
     public GameControl(){
-        startNewGame();
+        gameLocs = new GameLocations();
         Scanner scan = new Scanner(System.in);
 
-        Cave.println("Is this program running in Github Codepaces? (y/n)");
-        boolean workingInCodespaces = scan.next().equals("y");
-        if (!workingInCodespaces){ 
+        
+        if (!GraphicsEnvironment.isHeadless()){ 
             gui = new Gui("HUNT THE WUMPUS", 2560, 1440, gameLocs); 
         }
 
@@ -43,55 +38,60 @@ public  class GameControl {
     ///////////////////////////////////////////////
 
     // start-up
-    public void startNewGame(){
-        gameLocs = new GameLocations();
-        cave = new Cave();
-        player = new Player();
-    }
-
-    public Cave getCave(){
-        return this.cave;
+    public void getCave(){
+        
     }
 
     // background
     public void passTurn(){
+        int input = 0;
         // player input for where to move
-        // move(location)
+        move(input);
+        //String[] checkForHazards = gameLocs.checkForHazards();
+        String[] hazards = gameLocs.getHazards();
+        for (String hazard : hazards){
+            if (hazard.equals("Wumpus"))
+                wumpusEncounter();
+            else if (hazard.equals("Bat"))
+                batEncounter();
+            else if (hazard.equals("Pit")){
+                pitEncounter();
+            }
+
+        }
         // what happens in new room
         // actions
     }
 
     public void gameEnd(){
-        //What are the game-ending conditions?    
+
     }
+
+    private void wumpusEncounter(){}
+
+    private void batEncounter(){}
+
+    private void pitEncounter(){}
 
     // player input
     public void move(int playerInput){
-        if(cave.canMove(gameLocs.getPlayerLoc(), playerInput)){
-            int oldLoc = gameLocs.getPlayerLoc();
-            gameLocs.setPlayerLoc(playerInput);
-            //gui.playerMove(oldLoc, gameLocs.getPlayerLoc());
-        } else {
-            //gui.invalidMove();
-        }
+        /**if (gameLocs.setPlayerLoc())
+            gui.move(playerInput);
+        else
+            gui.failMove();
+        **/
     }
 
-    // Checks if the romm exists and if it does depletes arrow by one. the effects
-    // are undecided in where they should go. Is the wumpus in the room? etc.
     public void shootArrow(){
-        // button pressed -> gui calls this method -> this executes
-        if(isValid()){
-            player.setArrows(player.getArrows()-1);
-            if(hitsWumpus()); //do something
-        }
+
     }
 
     public void getStore(){
-        // wtf is a store?
+
     }
 
-    public void buyArrows(int num){
-        this.player.setArrows(player.getArrows()+num);
+    public void buyArrows(){
+
     }
 
     public void openHintUI(){
@@ -101,32 +101,5 @@ public  class GameControl {
     public void openTriviaUI(){
 
     }
-
-    public boolean isValid(){
-        /*
-         * In reality, this should go like this: the scanner sees where the player is
-         * Checks if player's direction is facing an open door
-         * if it is, the method should return true.
-         * An alternative is to somehow use a gui, but that's known but to god.
-        */
-        return true;
-    }
-
-    public void checkIfPlayerInTrap(){
-        this.playerInTrap = true;
-    }
-
-    public boolean hitsWumpus(){
-        // If the arrow is shot into the room the wumpus is in.
-        return true;
-    }
-   
-
-
-
-
-
-
-
 }
 
