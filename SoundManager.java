8267f58dd5595834193ff7818f5c4c7bfbb0cba0
TokiYoung <1113179@apps.nsd.org>
2024-05-20 14:17:59 -0700

    Added my explanation for SoundManager

--- SoundManager.java
+++ SoundManager.java
@@ -1,4 +1,12 @@
+//Toki
 public class SoundManager {
+    /*
+     * I assume the general purpose for this class is to play sounds
+     * when something happens. Most likely going to be used by GameControl.
+     * I guess this class will just have a bunch of sounds in a list
+     * and have a method that'll iterate over the list
+     * to find the correct sfx for whatever the GControl needed.
+     */
     public SoundManager(){
 
     }
