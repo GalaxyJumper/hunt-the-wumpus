29363f6f6b8b6a9a933fdcbbc3ad894f1be4d799
TokiYoung <1113179@apps.nsd.org>
2024-05-24 13:59:08 -0700

    Added basic wumpus functionality

--- Wumpus.java
+++ Wumpus.java
@@ -2,13 +2,21 @@
 
 public class Wumpus {
     // fields
-    
+    private int health;
     
 
     // Contructors
     public Wumpus(){
-            
+        this.health = 4;
     }
 
     // methods
+    public int getHealth(){
+        return this.health;
+    }
+
+    public void setHealth(int newVal) {
+        this.health = newVal;
+    }
+
 }
