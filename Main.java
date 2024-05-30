a33cb3904d51abdb3d052be67b13a0d2939a1520
kavya-tayall <2005879@apps.nsd.org>
2024-05-30 11:57:37 -0700

    idk

--- Main.java
+++ Main.java
@@ -10,10 +10,10 @@ public class Main {
         GameControl freak = new GameControl();
         
         //making sure highscore works! 
-        String[] test = new String[] {"bobby", "3", "2", "3", "7", "cave"};
-        Player player = new Player(test);
-        HighScore highscore = new HighScore(player);
-        highscore.endOfGame();
+        //String[] test = new String[] {"bobby", "3", "2", "3", "7", "cave"};
+        //Player player = new Player(test);
+        //HighScore highscore = new HighScore(player);
+        //highscore.endOfGame();
 
         // This should always run at the end of main!!!!
         Cave.println("Your code works ~*,`");
