7941d9fbb12fcd6d42c0ded1082a1182f97da8ea
TokiYoung <1113179@apps.nsd.org>
2024-05-31 14:14:46 -0700

    added new sounds mangager

--- SoundManager.java
+++ SoundManager.java
@@ -1,70 +1,62 @@
-//Toki
-//Nick
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.net.MalformedURLException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
-
-import javax.sound.sampled.*;
-
-//All audio clips need to be .wav!!!!
-
+/**
+ * Handles playing, stoping, and looping of sounds for the game.
+ * @author Tyler Thomas
+ *
+ */
 public class SoundManager {
-    private File f;
-    private ArrayList<File> sounds;
     private Clip clip;
-
-    /*
-     * I assume the general purpose for this class is to play sounds
-     * when something happens. Most likely going to be used by GameControl.
-     * I guess this class will just have a bunch of sounds in a list
-     * and have a method that'll iterate over the list
-     * to find the correct sfx for whatever the GControl needed.
-     * Memoirize the indecies of the sfx.
-     */
-    public SoundManager() throws FileNotFoundException{
-        this.sounds = new ArrayList<File>();
-        
-
-        this.sounds.add(new File("./gameOver.wav"));
-        this.sounds.add(new File("./dissapointment.wav"));
-        this.sounds.add(new File("./correctAnswer"));
-        this.sounds.add(new File("./caveNoise.wav"));
-        this.sounds.add(new File("./wrongAnswer.wav"));
-
+    public SoundManager(String fileName) {
+        // specify the sound to play
+        // (assuming the sound can be played by the audio system)
+        // from a wave File
         try {
-            
-        } catch (Exception e) {
-            // TODO: handle exception
+            File file = new File(fileName);
+            if (file.exists()) {
+                AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+             // load the sound into memory (a Clip)
+                clip = AudioSystem.getClip();
+                clip.open(sound);
+            }
+            else {
+                throw new RuntimeException("Sound: file not found: " + fileName);
+            }
         }
-        
-    }
-
-
-    
-    public static void playSound(File f){
-        try {
-            File file = f;
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
-            clip = AudioSystem.getClip();
-            clip.open();
-            clip.start();
-        } catch (Exception FileNotFoundException) {
-            System.out.println("Falaed");
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Malformed URL: " + e);
+        }
+        catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Unsupported Audio File: " + e);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Input/Output Error: " + e);
+        }
+        catch (LineUnavailableException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Line Unavailable Exception Error: " + e);
         }
 
-        
+    // play, stop, loop the sound clip
     }
-    public static void stopSound() {
-            clip.flush();
-            clip.close();
+    public void play(){
+        clip.setFramePosition(0);  // Must always rewind!
+        clip.start();
     }
-
-    public File getSfx(int index){
-        return this.sounds.get(index);
+    public void loop(){
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
-
-}
-
+    public void stop(){
+            clip.stop();
+        }
+    }
