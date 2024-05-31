3e0b57bb6560ac10bf0d5c01ac65b956851af528
TokiYoung <1113179@apps.nsd.org>
2024-05-31 13:57:11 -0700

    added filepaths

--- SoundManager.java
+++ SoundManager.java
@@ -2,13 +2,17 @@
 //Nick
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.sound.sampled.*;
 
 //All audio clips need to be .wav!!!!
 
 public class SoundManager {
     private File f;
+    private ArrayList<File> sounds;
     /*
      * I assume the general purpose for this class is to play sounds
      * when something happens. Most likely going to be used by GameControl.
@@ -16,7 +20,14 @@ public class SoundManager {
      * and have a method that'll iterate over the list
      * to find the correct sfx for whatever the GControl needed.
      */
+    public SoundManager() throws FileNotFoundException{
+        this.sounds = new ArrayList<File>();
 
+        this.sounds.add(new File("C:\Users\1113179\Hunt-the-wumpus\hunt-the-wumpus\gameOver.wav"));
+        this.sounds.add(new File("C:\Users\1113179\Hunt-the-wumpus\hunt-the-wumpus\dissapointment.wav"));
+        this.sounds.
+        
+    }
     
     public static void playSound(){
         clip.start();
