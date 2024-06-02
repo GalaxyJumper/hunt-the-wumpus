cd272325f751da7942164d8b7a7a5d6b75b3efde
Nick-Lennon <1085280@apps.nsd.org>
2024-06-02 13:51:47 -0700

    made a sound manager

--- Main.java
+++ Main.java
@@ -6,12 +6,14 @@ public class Main {
             throws FontFormatException, IOException {
         System.setProperty("sun.java2d.uiScale", "0.7");
         new GameControl();
-        
-        //making sure highscore works! 
-        //String[] test = new String[] {"bobby", "3", "2", "3", "7", "cave"};
-        //Player player = new Player(test);
-        //HighScore highscore = new HighScore(player);
-        //highscore.endOfGame();
+        SoundManager sm = new SoundManager();
+        sm.play(0);
+
+        // making sure highscore works!
+        // String[] test = new String[] {"bobby", "3", "2", "3", "7", "cave"};
+        // Player player = new Player(test);
+        // HighScore highscore = new HighScore(player);
+        // highscore.endOfGame();
 
         // This should always run at the end of main!!!!
         Cave.println("Your code works ~*,`");
