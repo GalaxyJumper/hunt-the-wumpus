8e47d87d0e37fd1ffb919cf57523252614236580
JoshSLennon <1085259@apps.nsd.org>
2024-06-07 20:52:15 -0700

    win

--- GameControl.java
+++ GameControl.java
@@ -328,7 +328,7 @@ public  class GameControl {
     // leaderboard display sequence
     public void gameEnd(boolean won){
         player.calcScore(won);
-        
+        sManager.playSound((won)? 0 : 5);
         String[][] leaderboardInfo = scores.endOfGame();
         
         gui.gameEndSequence(leaderboardInfo, won, scores.getPlayerIndex());
